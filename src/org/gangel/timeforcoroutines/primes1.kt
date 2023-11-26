155dc33e1115c5c6f28ec01c051ae2cead778ce1
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2023-11-26 11:47:38 +0100

    Fix coroutines for primes generation

--- src/org/gangel/timeforcoroutines/primes1.kt
+++ src/org/gangel/timeforcoroutines/primes1.kt
@@ -1,26 +1,37 @@
 package org.gangel.timeforcoroutines
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun CoroutineScope.numbersFrom(start: Int) = produce {
+    var x = start
+    while (true) {
+        println("Going to produce next number: $x")
+        send(x++)
+    } // infinite stream of integers from start
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun CoroutineScope.filter(numbers: ReceiveChannel<Int>, prime: Int) = produce {
+    for (x in numbers) {
+        if (x % prime != 0) {
+            send(x)
+        }
+    }
+}
 
 @OptIn(ExperimentalCoroutinesApi::class)
 fun main() = runBlocking(Dispatchers.Default) {
 
-    fun CoroutineScope.numbersFrom(start: Int) = produce {
-        var x = start
-        while (true) send(x++) // infinite stream of integers from start
-    }
-
-    fun CoroutineScope.filter(numbers: ReceiveChannel<Int>, prime: Int) = produce {
-        for (x in numbers) if (x % prime != 0) send(x)
-    }
-
     //sampleStart
     var cur = numbersFrom(2)
     for (i in 1..200) {
-        val prime = cur.receive()
-        println(prime)
-        cur = filter(cur, prime)
+        val primeCandidate = cur.receive()
+        println("Received number: $primeCandidate")
+        cur = filter(cur, primeCandidate)
     }
     coroutineContext.cancelChildren() // cancel all children to let main finish
 //sampleEnd
